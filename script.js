5d447e0f680803b7bd2a7e45fcc1b862c44becb3
Faaiq <66476388+verhaxity@users.noreply.github.com>
2024-02-11 16:43:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+gsap.from(".nav-logo, .navlinks > a", 1.5, {
+    top:"30px",
+    opacity: 0,
+    ease: "power4.inOut",
+    delay: 1,
+    stagger: {
+        amount: 0.3,
+    },
+
+});
+
+gsap.from("h1", 1.5, {
+    y:50,
+    opacity: 0,
+    ease: "power4.inOut",
+    delay: 1.5,
+    stagger: {
+        amount: 0.3,
+    },
+
+});
+
+gsap.from(".play-wrapper, .pattern, .copy", 1.5, {
+    scaleY: 0,
+    ease: "power3.inOut",
+    delay: 1,
+    stagger: {
+        amount: 0.3,
+    },
+    delay: 2.5,
+
+});
+
+gsap.from(".hr", 1.5, {
+    width:"0",
+    ease: "power3.inOut",
+    delay: 3,
+
+});
+
+gsap.from(".btns", 1.5, {
+    X:50,
+    opacity: 0,
+    ease: "power3.inOut",
+    delay: 3,
+
+});
+
+gsap.from(".play-btn", 1.5, {
+    scale: 0,
+    opacity: 0,
+    ease: "power3.inOut",
+    delay: 3,
+
+});
+
+gsap.from(".hero-wrapper", 1.5, {
+    width: "100%",
+    ease: "power3.inOut",
+    delay: 3,
+
+});
+
+gsap.from(".arrow", 1.5, {
+    scale: 0,
+    ease: "power3.inOut",
+    delay: 3,
+
+});
+
+gsap.from(".marquee", 1.5, {
+    bottom: "-10rem",
+    ease: "power3.inOut",
+    delay: 4,
+
+});
